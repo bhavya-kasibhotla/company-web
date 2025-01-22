345afeee38eb1032d9467fb991a978068efe98bd
bhavya-kasibhotla <152894816+bhavya-kasibhotla@users.noreply.github.com>
2025-01-23 00:35:24 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas
 st.set_page_config(layout="wide")
 st.header("The Best Company")
-st.write("""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""")
+st.write(" It’s an opportunity to tell your brand’s story, share your vision or mission, introduce team members, and outline your history and achievements. ")
 st.subheader("Our Team")
 col1,col2,col3=st.columns(3)
 df=pandas.read_csv("data (1).csv")
@@ -10,7 +10,7 @@ with col1:
     for index,row in df[:4].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
-        st.image("images/" + row['image'])
+        
 
 
 
@@ -18,10 +18,8 @@ with col2:
     for index,row in df[4:8].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
-        st.image("images/" + row['image'])
 
 with col3:
     for index,row in df[8:].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
-        st.image("images/" + row['image'])
